477b53ce33e8a484cf0cb4fc99d514a9e89ae606
stylessh <stylessh@gmail.com>
2022-11-03 19:47:25 -0400

    feat: using front camera

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import Webcam from "react-webcam";
 
+const videoConstraints = {
+  facingMode: { exact: "environment" },
+};
+
 const Index = () => {
-  return <Webcam className="webcam" screenshotFormat="image/png" />;
+  return (
+    <Webcam
+      className="webcam"
+      screenshotFormat="image/png"
+      videoConstraints={videoConstraints}
+    />
+  );
 };
 
 export default Index;
